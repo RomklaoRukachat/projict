6b7fde1156ac35decda71765d1659ce9798603e6
RomklaoRukachat <151832143+RomklaoRukachat@users.noreply.github.com>
2024-01-23 13:42:58 +0700

    2301672
    23

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,25 +1,35 @@
 exports.register = (req, res, next) => {
-    const { email, password } = req.body;
-  
-    //   logic
-  
-    res.json({ email, password });
-  };
-  
-  exports.login = (req, res, next) => {
-    res.json({ message: "Login" });
-  };
-  
-  exports.forgetPassword = (req, res, next) => {
-      const {email} = req.body;
-      // gen token -> สร้าง link -> ส่ง email
-    res.json({ message: "Forget password" });
-  };
-  
-  exports.verifyForgetPassword = (req, res, next) => {
-    res.json({ message: "Verify forget password" });
-  };
-  
-  exports.resetPassword = (req, res, next) => {
-    res.json({ message: "Reset password" });
-  };
+  const { email, password } = req.body;
+
+  //   logic
+
+  res.json({ email, password });
+};
+
+exports.login = (req, res, next) => {
+  const { email, password } = req.body;
+  res.json({ email, password });
+};
+
+exports.forgetPassword = (req, res, next) => {
+  const { email } = req.body;
+  // gen token -> สร้าง link -> ส่ง email
+  res.json({ email });
+};
+
+// https://api.codecamp.com/auth/forget-password/kdjfkdjfkdjkfjd
+exports.verifyForgetPassword = (req, res, next) => {
+  const { token } = req.params;
+  // logic check token
+  // redirect reset password -> ติด token
+  res.json({ token });
+};
+
+exports.resetPassword = (req, res, next) => {
+  const { token } = req.params;
+  const { password } = req.body;
+  // check token
+  // เปลี่ยน Password
+  // เก็บ password ใหม่ ลง db
+  res.json({ token, password });
+};
