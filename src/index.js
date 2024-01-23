7b0c2f1a7ed81dcd3c8d6d9d4303496e6f6c642d
RomklaoRukachat <151832143+RomklaoRukachat@users.noreply.github.com>
2024-01-23 09:34:48 +0700

    day1

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
+const errorHandler = require("./middlewares/error");
+const notFoundHandler = require("./middlewares/notFound");
 
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-const port =process.env.PORT
-app.listen(port, ()=> console.log(`listening on post ${port}`))
+app.use(errorHandler);
+app.use("*", notFoundHandler);
+
+const port = process.env.PORT;
+app.listen(port, () => {
+  console.log("Server run on port" + " " + port);
+});
